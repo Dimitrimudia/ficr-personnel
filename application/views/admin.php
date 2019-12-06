6e35d939d9674e482f51baf34c80836df45599c7
Dimitri Mudiangondo <dimitrimudia@gmail.com>
2019-12-06 18:55:36 +0100

    ''

--- application/views/admin.php
+++ application/views/admin.php
@@ -117,49 +117,76 @@
     </div>
     <div class="row">
         <div class="col-lg-12">
-            <div class="ibox">
+            <div class="ibox float-e-margins">
+                <div class="ibox-title">
+                    <h5>Basic Data Tables example with responsive plugin</h5>
+                    <div class="ibox-tools">
+                        <a class="collapse-link">
+                            <i class="fa fa-chevron-up"></i>
+                        </a>
+                        <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                            <i class="fa fa-wrench"></i>
+                        </a>
+                        <ul class="dropdown-menu dropdown-user">
+                            <li><a href="#">Config option 1</a>
+                            </li>
+                            <li><a href="#">Config option 2</a>
+                            </li>
+                        </ul>
+                        <a class="close-link">
+                            <i class="fa fa-times"></i>
+                        </a>
+                    </div>
+                </div>
                 <div class="ibox-content">
                     <?php if(!empty($agents)):?>
-                        <table class="footable table table-stripped" data-page-size="8" data-filter=#filter>
-                             <input type="text" class="form-control input-sm m-b-xs" id="filter" placeholder="Tapez un text">
-                            <thead>
-                                <tr>
-                                    <th>ID</th>
-                                    <th>Noms</th>
-                                    <th>Etat-civil</th>
-                                    <th>Statut</th>
-                                    <th>Fonction</th>
-                                    <th data-hide="phone">T&eacute;l&eacute;phone</th>
-                                    <th >E-mail</th>
-                                    <th class="text-right" data-sort-ignore="true"><i class="fa fa-pencil-square-o"></i></th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                <?php $i = 1 ; foreach($agents as $agent):?>
-                                    <tr id="line<?php echo $agent->Id_0;?>">
-                                        <td><?php echo $i;?></td>
-                                        <td><?php echo $agent->Nom_7.' '.$agent->Postnom_8.' '.$agent->Prenom_9;?></td>
-                                        <td><?php echo $maritals[$agent->EtatCivil_12];?></td>
-                                        <td><?php echo $status[$agent->Statut_3];?></td>
-                                        <td><?php echo $agent->Fonction_14;?></td>
-                                        <td>
-                                            <?php echo $agent->Telephone_13;?><br>
-                                        </td>
-                                        <td><?php echo $agent->Email_16;?></td>
-                                        <td align='center'><a style="cursor:pointer; text-decoration: none;color:#1ab394;" onclick="edit(<?php echo $agent->Id_0;?>)"><i class="fa fa-pencil"></i></a></td>
+                        <div class="table-responsive">
+                            <table class="table table-striped table-bordered table-hover dataTables-example">
+                                <thead>
+                                    <tr>
+                                        <th>ID</th>
+                                        <th>Noms</th>
+                                        <th>Etat-civil</th>
+                                        <th>Statut</th>
+                                        <th>Fonction</th>
+                                        <th data-hide="phone">T&eacute;l&eacute;phone</th>
+                                        <th >E-mail</th>
+                                        <th class="text-right" data-sort-ignore="true"><i class="fa fa-pencil-square-o"></i></th>
                                     </tr>
-                                    <?php  $i++;  ?>
-                                <?php endforeach;?>
-                            </tbody>
-                            <tfoot>
-                                <tr>
-                                    <td colspan="9">
-                                        <ul class="pagination pull-right"></ul>
-                                    </td>
-                                </tr>
-                            </tfoot>
-                        </table>
+                                </thead>
+                                <tbody>
+                                    <?php $i = 1 ; foreach($agents as $agent):?>
+                                        <tr id="line<?php echo $agent->Id_0;?>">
+                                            <td><?php echo $i;?></td>
+                                            <td><?php echo $agent->Nom_7.' '.$agent->Postnom_8.' '.$agent->Prenom_9;?></td>
+                                            <td><?php echo $maritals[$agent->EtatCivil_12];?></td>
+                                            <td><?php echo $status[$agent->Statut_3];?></td>
+                                            <td><?php echo $agent->Fonction_14;?></td>
+                                            <td>
+                                                <?php echo $agent->Telephone_13;?><br>
+                                            </td>
+                                            <td><?php echo $agent->Email_16;?></td>
+                                            <td align='center'><a style="cursor:pointer; text-decoration: none;color:#1ab394;" onclick="edit(<?php echo $agent->Id_0;?>)"><i class="fa fa-pencil"></i></a></td>
+                                        </tr>
+                                        <?php  $i++;  ?>
+                                    <?php endforeach;?>
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                       <th>ID</th>
+                                        <th>Noms</th>
+                                        <th>Etat-civil</th>
+                                        <th>Statut</th>
+                                        <th>Fonction</th>
+                                        <th data-hide="phone">T&eacute;l&eacute;phone</th>
+                                        <th >E-mail</th>
+                                        <th class="text-right" data-sort-ignore="true"><i class="fa fa-pencil-square-o"></i></th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
                     <?php else:?>
+                    
                     <?php endif;?>
                     
                 </div>
@@ -169,7 +196,33 @@
 </div>
 <script src="<?php echo base_url('assets/js/plugins/footable/footable.all.min.js'); ?>" type="text/javascript"></script>
 <script type="text/javascript">
-    $('.footable').footable();
+    $(document).ready(function(){
+            $('.dataTables-example').DataTable({
+                pageLength: 25,
+                responsive: true,
+                dom: '<"html5buttons"B>lTfgitp',
+                buttons: [
+                    { extend: 'copy'},
+                    {extend: 'csv'},
+                    {extend: 'excel', title: 'Liste des agents'},
+                    {extend: 'pdf', title: 'Liste des agents'},
+
+                    {extend: 'print',
+                     customize: function (win){
+                            $(win.document.body).addClass('white-bg');
+                            $(win.document.body).css('font-size', '10px');
+
+                            $(win.document.body).find('table')
+                                    .addClass('compact')
+                                    .css('font-size', 'inherit');
+                    }
+                    }
+                ]
+
+            });
+
+        });
+        
     $("#add").click(function(){
            Add();
     });
--- application/views/header/admin.php
+++ application/views/header/admin.php
@@ -8,7 +8,7 @@
     <link href="<?php echo base_url('assets/css/plugins/footable/footable.core.css'); ?>" rel="stylesheet" type="text/css" />
     <link href="<?php echo base_url('assets/css/plugins/toastr/toastr.min.css'); ?>" rel="stylesheet" type="text/css" />
     <link href="<?php echo base_url('assets/js/plugins/gritter/jquery.gritter.css'); ?>" rel="stylesheet" type="text/css" />
-    <link href="<?php echo base_url('assets/css/plugins/footable/footable.core.css'); ?>" rel="stylesheet" type="text/css" />
+    <link href="<?php echo base_url('assets/css/plugins/dataTables/datatables.min.css'); ?>" rel="stylesheet" type="text/css" />
     <link href="<?php echo base_url('assets/css/plugins/datapicker/datepicker3.css'); ?>" rel="stylesheet" type="text/css" />
      <link href="<?php echo base_url('assets/css/animate.css'); ?>" rel="stylesheet" type="text/css" />
     <link href="<?php echo base_url('assets/css/style.css'); ?>" rel="stylesheet" type="text/css" />
